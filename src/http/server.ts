b63d371f4510041a93f53e41c199ca622b801463
Leozera2k <leofsousa98@gmail.com>
2024-09-13 18:52:45 -0300

    criado primeira função

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,22 +1,36 @@
 import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+ type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
 import { createGoal } from '../functions/create-goal'
 import z from 'zod'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-app.post('/goals', async request => {
-  const createGoalSchema = z.object({
-    tittle: z.string(),
-    desiredWeeklyFrequency: z.number().int().min(1).max(7),
-  })
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
-  const body = createGoalSchema.parse(request.body)
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        tittle: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+const { tittle, desiredWeeklyFrequency } = request.body
 
-  await createGoal({
-    tittle: body.tittle,
-    desiredWeeklyFrequency: body.desiredWeeklyFrequency,
-  })
-})
+    await createGoal({
+      tittle,
+      desiredWeeklyFrequency,
+    })
+  }
+)
 
 app
   .listen({
